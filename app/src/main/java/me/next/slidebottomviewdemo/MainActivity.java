8cfa5b63eaf23b6dec44e8585ac38627c47df615
NeXT <kingideayou@qq.com>
2015-08-03 23:54:47 +0800

    添加底层视图 DarkFrameLayout

--- app/src/main/java/me/next/slidebottomviewdemo/MainActivity.java
+++ app/src/main/java/me/next/slidebottomviewdemo/MainActivity.java
@@ -1,38 +1,42 @@
 package me.next.slidebottomviewdemo;
 
-import android.support.v7.app.ActionBarActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
+import me.next.slidebottomview.SlideBottomView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
+    private String webUrl = "http://www.zhihu.com/question/29416073/answer/44340933";
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SlideBottomView sbv = (SlideBottomView) findViewById(R.id.sbv);
+        webView = (WebView) findViewById(R.id.wv_main);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(webUrl);
+        webView.setWebChromeClient(new WebChromeClient());
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
+
 }
--- slidebottomview/src/main/java/me/next/slidebottomview/SlideBottomView.java
+++ slidebottomview/src/main/java/me/next/slidebottomview/SlideBottomView.java
@@ -1,13 +1,22 @@
 package me.next.slidebottomview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.FrameLayout;
 
 /**
  * Created by NeXT on 15/8/3.
  */
 public class SlideBottomView extends FrameLayout {
+
+    private static final int DEFAULT_BACKGROUND_ID = -1;
+
+    private int mBackgroundId;
+
+    private Context mContext;
+    private DarkFrameLayout mDarkFrameLayout;
 
     public SlideBottomView(Context context) {
         this(context, null);
@@ -19,6 +28,23 @@ public class SlideBottomView extends FrameLayout {
 
     public SlideBottomView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SlideBottomView, defStyleAttr, 0);
+
+        mBackgroundId = a.getResourceId(R.styleable.SlideBottomView_sbv_background_layout, DEFAULT_BACKGROUND_ID);
+
+        a.recycle();
+
+        mContext = context;
+        initBackgroundView();
+    }
+
+    private void initBackgroundView() {
+        if (mBackgroundId != -1) {
+            mDarkFrameLayout = new DarkFrameLayout(mContext);
+            mDarkFrameLayout.addView(LayoutInflater.from(mContext).inflate(mBackgroundId, null));
+            addView(mDarkFrameLayout);
+        }
     }
 
 }
